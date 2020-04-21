ba24d070c39cc4c6e5f5d23c125451ca5abaf525
Dorian Isaac Vallecillo Calderón <dorianivc1@gmail.com>
2020-04-21 07:27:08 -0600

    Cambio de mensaje
    Signed-off-by: Dorian Isaac Vallecillo Calderón <dorianivc1@gmail.com>

--- AddClient/run.ps1
+++ AddClient/run.ps1
@@ -17,7 +17,7 @@ $body = "This HTTP triggered function executed successfully. Pass a name in the 
 if ($name) {
         
     Invoke-WebRequest -URI https://72tgrow1b2.execute-api.us-east-1.amazonaws.com/Test1/addclient -ContentType "application/json" -Method GET -Body (@{"cedula" = $name;} | ConvertTo-Json)
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+    $body = "Hello, the ID $name were injected successfully into the database"
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.

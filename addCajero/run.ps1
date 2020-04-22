42107a264179e114fc2505c335b57f01fce865dd
Dorian Isaac Vallecillo Calderón <dorianivc1@gmail.com>
2020-04-21 19:20:03 -0600

    change

--- addCajero/run.ps1
+++ addCajero/run.ps1
@@ -23,5 +23,5 @@ if ($name) {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body5
+    Body = $body
 })
